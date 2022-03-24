23633394f5c5c1c21c3426259c3dfb87dfbd7e5f
Konrad Kubczyk <konrad@kubczyk.com>
2022-03-24 10:45:58 +0100

    Remove progress logging in console

--- scripts/scrollprogress.js
+++ scripts/scrollprogress.js
@@ -1,7 +1,6 @@
 let scrollProgress = () => {
 	let progress = document.documentElement.scrollTop / (document.documentElement.offsetHeight - document.documentElement.clientHeight) * 100;
 	document.getElementById('navbar').style = `background: linear-gradient(to right, #e2eeff ${progress}%, #fefefe ${progress}%);`;
-	console.log(progress);
 }
 
 document.addEventListener('scroll', scrollProgress);
